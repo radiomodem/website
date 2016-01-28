1abc6d9d170c2a22330c06297e2ccf9c3f2408cc
Kasper Isager <kasperisager@gmail.com>
2016-01-28 07:24:19 +0100

    Update build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,94 @@
 'use strict';
 
+const fs = require('fs');
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
+const gulpsmith = require('gulpsmith');
 const del = require('del');
+const $ = require('gulp-load-plugins')();
+const $$ = require('load-metalsmith-plugins')();
+const _ = require('lodash');
+const async = require('async');
+const humans = require('humans-generator');
+const robots = require('robots-generator');
 
-gulp.task('img', ['img:clean'], function () {
-  return gulp.src('img/**')
-    .pipe($.plumber())
-    .pipe($.changed('dist/img'))
-    .pipe($.imagemin({
-      svgoPlugins: [
-        {removeTitle: true},
-        {removeDesc: true}
-      ]
+gulp.task('img', ['img:clean'], () => gulp.src('img/**')
+  .pipe($.plumber())
+  .pipe($.changed('dist/img'))
+  .pipe($.imagemin({
+    svgoPlugins: [
+      {removeTitle: true},
+      {removeDesc: true}
+    ]
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe($.size({
+    title: 'img',
+    showFiles: true
+  }))
+);
+
+gulp.task('img:clean', () => del(['dist/img']));
+
+gulp.task('html', ['html:clean'], () => gulp.src('html/**/*.{md,html}')
+  .pipe($.plumber())
+  .pipe($.frontMatter().on('data', file => {
+    _.assign(file, file.frontMatter);
+  }))
+  .pipe(gulpsmith()
+    .use($$.markdown())
+    .use($$.permalinks(':title'))
+    .use($$.layouts({
+      engine: 'ejs',
+      directory: 'tpl'
     }))
-    .pipe(gulp.dest('dist/img'))
-    .pipe($.size({
-      title: 'img',
-      showFiles: true
-    }));
-});
-
-gulp.task('img:clean', function (done) {
-  del(['dist/img'], done);
-});
-
-gulp.task('html', ['html:clean'], function () {
-  const gulpsmith = require('gulpsmith');
-  const _ = require('lodash');
-  const $$ = require('load-metalsmith-plugins')();
-
-  return gulp.src('html/**/*.{md,html}')
-    .pipe($.plumber())
-    .pipe($.frontMatter().on('data', function (file) {
-      _.assign(file, file.frontMatter);
+    .use($$.inPlace({
+      engine: 'ejs'
     }))
-    .pipe(gulpsmith()
-      .use($$.markdown())
-      .use($$.permalinks(':title'))
-      .use($$.layouts({
-        engine: 'ejs',
-        directory: 'tpl'
-      }))
-      .use($$.inPlace({
-        engine: 'ejs'
-      }))
-    )
-    .pipe($.minifyHtml())
-    .pipe(gulp.dest('dist'))
-    .pipe($.livereload())
-    .pipe($.size({
-      title: 'html',
-      showFiles: true
-    }));
-});
+  )
+  .pipe($.htmlmin())
+  .pipe(gulp.dest('dist'))
+  .pipe($.livereload())
+  .pipe($.size({
+    title: 'html',
+    showFiles: true
+  }))
+);
 
-gulp.task('html:clean', function (done) {
-  del(['dist/**/*.html'], done);
-});
+gulp.task('html:clean', () => del(['dist/**/*.html']));
 
-gulp.task('copy', function () {
-  return gulp.src([
-    'favicon.ico',
-    'CNAME'
-  ], {
-    base: './'
-  })
-    .pipe($.plumber())
-    .pipe($.changed('dist'))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('copy', () => gulp.src([
+  'favicon.ico',
+  'CNAME'
+], {
+  base: './'
+})
+  .pipe($.plumber())
+  .pipe($.changed('dist'))
+  .pipe(gulp.dest('dist'))
+);
 
-gulp.task('meta', ['meta:clean'], function (done) {
-  const _ = require('lodash');
-  const async = require('async');
-  const humans = require('humans-generator');
-  const robots = require('robots-generator');
+gulp.task('meta', ['meta:clean'], done => async.parallel([
+  next => {
+    humans(require('./humans.json'), (err, content) => {
+      if (err) {
+        return;
+      }
 
-  async.parallel([
-    function (next) {
-      humans(_.extend(require('./humans.json'), {
-        out: 'dist/humans.txt',
-        callback: next
-      }));
-    },
-    function (next) {
-      robots(_.extend(require('./robots.json'), {
-        out: 'dist/robots.txt',
-        callback: next
-      }));
-    }
-  ], done);
-});
+      fs.writeFile('dist/humans.txt', content.join('\n'), next);
+    });
+  },
+  next => {
+    robots(require('./robots.json'), (err, content) => {
+      if (err) {
+        return;
+      }
 
-gulp.task('meta:clean', function (done) {
-  del(['dist/humans.txt', 'dist/robots.txt'], done);
-});
+      fs.writeFile('dist/robots.txt', content.join('\n'), next);
+    });
+  }
+], done));
+
+gulp.task('meta:clean', () => del(['dist/humans.txt', 'dist/robots.txt']));
 
 gulp.task('build', [
   'img',
@@ -106,7 +99,7 @@ gulp.task('build', [
 
 gulp.task('default', ['build']);
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', ['build'], () => {
   $.livereload.listen();
 
   gulp.watch([
@@ -115,29 +108,25 @@ gulp.task('watch', ['build'], function () {
   ], ['html']);
 });
 
-gulp.task('serve', ['watch'], function () {
-  return gulp.src('dist')
-    .pipe($.plumber())
-    .pipe($.webserver({
-      open: true,
-      livereload: true
-    }));
-});
+gulp.task('serve', ['watch'], () => gulp.src('dist')
+  .pipe($.plumber())
+  .pipe($.webserver({
+    open: true,
+    livereload: true
+  }))
+);
 
-gulp.task('deploy', ['deploy:clean', 'build'], function () {
-  return gulp.src('dist/**/*')
-    .pipe($.plumber())
-    .pipe($.ghPages({
-      remoteUrl: 'git@github.com:radio-modem/radio-modem.github.io.git',
-      branch: 'master',
-      cacheDir: '.tmp',
-      force: true
-    }))
-    .pipe($.size({
-      title: 'deploy'
-    }));
-});
+gulp.task('deploy', ['deploy:clean', 'build'], () => gulp.src('dist/**/*')
+  .pipe($.plumber())
+  .pipe($.ghPages({
+    remoteUrl: 'git@github.com:radio-modem/radio-modem.github.io.git',
+    branch: 'master',
+    cacheDir: '.tmp',
+    force: true
+  }))
+  .pipe($.size({
+    title: 'deploy'
+  }))
+);
 
-gulp.task('deploy:clean', function (done) {
-  del(['.tmp'], done);
-});
+gulp.task('deploy:clean', () => del(['.tmp']));
